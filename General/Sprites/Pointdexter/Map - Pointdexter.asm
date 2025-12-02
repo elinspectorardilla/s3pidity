687c0ab971ab51765061b4fa9b2f2643e41ebf97
elinspectorardilla <fatguyinacouch@gmail.com>
2025-12-02 02:34:46 -0700

    Pointdexter mappings fix

--- General/Sprites/Pointdexter/Map - Pointdexter.asm
+++ General/Sprites/Pointdexter/Map - Pointdexter.asm
@@ -7,9 +7,9 @@ Frame_360E78:	dc.w 3
 		dc.b  $F8,   9,   0,   2, $FF, $F4
 		dc.b  $F8,   1,   0,   8,   0,  $C
 Frame_360E8C:	dc.w 3
-		dc.b  $F4,   1,   0,   0, $FF, $EC
+		dc.b  $F8,   1,   0,   0, $FF, $EC
 		dc.b  $F4,  $A,   0,  $A, $FF, $F4
-		dc.b  $F4,   1,   0,   8,   0,  $C
+		dc.b  $F8,   1,   0,   8,   0,  $C
 Frame_360EA0:	dc.w 3
 		dc.b  $F8,   1,   0,   0, $FF, $EC
 		dc.b  $F0,  $B,   0, $13, $FF, $F4
